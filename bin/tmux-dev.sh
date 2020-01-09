143d353ae16c85381bf355ed1b4d1a81d533c0fe
Brian Takita <brian.takita@gmail.com>
2020-01-09 03:35:06 -0500

    fix: bin/tmux-dev.sh: nix-shell https://github.com/holochain/holonix/archive/develop.tar.gz
    ↑ zomes/cogov/code/Cargo.lock

--- bin/tmux-dev.sh
+++ bin/tmux-dev.sh
@@ -5,12 +5,12 @@ DEV_DIR="${DEV_DIR:-$DEV_DIR_DEFAULT}"
 cd $DEV_DIR
 tmux rename-window cogov-dev
 tmux split-window -h $SHELL
-tmux send-keys 'nix-shell nix-shell https://github.com/holochain/holonix/archive/v0.0.60.tar.gz' 'C-m'
+tmux send-keys 'nix-shell https://github.com/holochain/holonix/archive/develop.tar.gz' 'C-m'
 tmux split-window -v $SHELL
 tmux send-keys 'tig' 'C-m'
 tmux select-pane -t 0
 tmux split-window -v $SHELL
-tmux send-keys 'nix-shell --run "hc run -i http" nix-shell https://github.com/holochain/holonix/archive/v0.0.60.tar.gz' 'C-m'
+tmux send-keys 'nix-shell --run "hc run -i http" https://github.com/holochain/holonix/archive/develop.tar.gz' 'C-m'
 tmux select-pane -t 0
 
 tmux select-window -t 0
