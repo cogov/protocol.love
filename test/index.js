b8ad043020d6d639e2b93c6fc81c4a734738adb9
Brian Takita <brian.takita@gmail.com>
2020-05-02 21:22:32 -0400

    test/index.js: enabled the rest of the test: parity with test/http.js
    + .nvmrc: 12.13.0
    
    test/package.json: ∋ "@ctx-core/object": "^8.0.8","esm": "^3.2.25"

--- test/index.js
+++ test/index.js
@@ -1,6 +1,9 @@
+require = require('esm')(module)
 const tryorama = require('@holochain/tryorama')
 const path = require('path')
 const fs = require('fs')
+const deepEqual = require('deep-equal')
+const { clone } = require('@ctx-core/object')
 // Point to your DNA file and give it a nickname.
 // The DNA file can either be on your filesystem...
 main()
@@ -23,77 +26,68 @@ async function main() {
 		await s.consistency()
 		const { person_address, person } = await assert_create_person(alice, t)
 		await assert_get_person(alice, t, { person_address, person })
-//		const { collective_address, collective } = await assert_create_collective(alice, t, { person_address })
-		// This next assertion times out
-//		await assert_get_collective_creator(alice, t, { collective_address, collective_creator: person })
-//		await assert_get_collective_people(alice, t, { collective_address, collective_people: [person] })
-//		await assert_get_collective(alice, t, { collective_address, collective })
-//		t.deepEqual(await _get_actions_result(alice, t, collective_address), {
-//			Ok: {
-//				collective_address: collective_address,
-//				actions: [
-//					_create_collective_action(collective),
-//					_set_collective_name_action(collective.name),
-//					_set_total_shares_action(collective.total_shares),
-//					_add_collective_person_action(person_address),
-//				]
-//			}
-//		})
-//		const {
-//			collective: collective__renamed,
-//			collective_address: collective_address__renamed
-//		} =
-//			await assert_set_collective_name(t, {
-//				collective_address,
-//				collective,
-//				name: 'Renamed Collective'
-//			})
-//		t.equal(collective_address, collective_address__renamed)
-//		t.notEqual(collective.name, collective__renamed.name)
-//		await assert_get_collective(alice, t, {
-//			collective_address: collective_address__renamed,
-//			collective: collective__renamed
-//		}, { timeout_ms: 5000 })
-//		t.deepEqual(await _get_actions_result(alice, t, collective_address), {
-//			Ok: {
-//				collective_address: collective_address,
-//				actions: [
-//					_create_collective_action(collective),
-//					_set_collective_name_action(collective.name),
-//					_set_total_shares_action(collective.total_shares),
-//					_add_collective_person_action(person_address),
-//					_set_collective_name_action(collective__renamed.name),
-//				]
-//			}
-//		})
-//		const {
-//			collective: collective__total_shares,
-//			collective_address: collective_address__total_shares
-//		} =
-//			await assert_set_collective_total_shares(alice, t, {
-//				collective_address,
-//				collective: collective__renamed,
-//				total_shares: 750000
-//			})
-//		t.equal(collective_address, collective_address__total_shares)
-//		t.notEqual(collective.total_shares, collective__total_shares.total_shares)
-//		await assert_get_collective(t, {
-//			collective_address: collective_address__total_shares,
-//			collective: collective__total_shares
-//		}, { timeout_ms: 5000 })
-//		t.deepEqual(await _get_actions_result(alice, t, collective_address), {
-//			Ok: {
-//				collective_address: collective_address,
-//				actions: [
-//					_create_collective_action(collective),
-//					_set_collective_name_action(collective.name),
-//					_set_total_shares_action(collective.total_shares),
-//					_add_collective_person_action(person_address),
-//					_set_collective_name_action(collective__total_shares.name),
-//					_set_total_shares_action(collective__total_shares.total_shares),
-//				]
-//			}
-//		})
+		const { collective_address, collective } =
+			await assert_create_collective(alice, t, {
+				admin_address: person_address,
+			})
+		await assert_get_collective_people(alice, t, {
+			collective_address,
+			collective_people: [person],
+		})
+		await assert_get_collective(alice, t, {
+			collective_address,
+			collective,
+		})
+		t.deepEqual(
+			await _get_actions_result(alice, t, collective_address),
+			{
+				Ok: {
+					collective_address: collective_address,
+					actions: [
+						_create_collective_action(collective),
+						_set_collective_name_action(collective.name),
+						_add_collective_person_action(person_address),
+					]
+				}
+			})
+		const {
+			collective: collective__renamed,
+			collective_address: collective_address__renamed
+		} =
+			await assert_set_collective_name(alice, t, {
+				collective_address,
+				collective,
+				name: 'Renamed Collective'
+			})
+		t.equal(collective_address, collective_address__renamed)
+		t.notEqual(collective.name, collective__renamed.name)
+		await assert_get_collective(alice, t, {
+			collective_address: collective_address__renamed,
+			collective: collective__renamed
+		}, { timeout_ms: 5000 })
+		t.deepEqual(await _get_actions_result(alice, t, collective_address),
+			{
+				Ok: {
+					collective_address: collective_address,
+					actions: [
+						_create_collective_action(collective),
+						_set_collective_name_action(collective.name),
+						_add_collective_person_action(person_address),
+						_set_collective_name_action(collective__renamed.name),
+					]
+				}
+			})
+		t.deepEqual(await _get_actions_result(alice, t, collective_address), {
+			Ok: {
+				collective_address: collective_address,
+				actions: [
+					_create_collective_action(collective),
+					_set_collective_name_action(collective.name),
+					_add_collective_person_action(person_address),
+					_set_collective_name_action(collective__renamed.name),
+				]
+			}
+		})
 	})
 	const report = await orchestrator.run()
 	console.log(report)
@@ -149,14 +143,13 @@ async function assert_get_person(player, t, { person_address, person }, opts = {
 		})
 	}
 }
-async function assert_create_collective(player, t, { person_address }) {
+async function assert_create_collective(player, t, { admin_address }) {
 	const create_collective_result =
 		await player_call(player,
 			'create_collective', {
 				collective: {
-					person_address,
+					admin_address,
 					name: 'Flower of Life Collective',
-					total_shares: 500000
 				}
 			}
 		)
@@ -168,31 +161,11 @@ async function assert_create_collective(player, t, { person_address }) {
 	t.assert(collective_address, 'collective_address should be truthy')
 	t.deepEqual(collective, {
 		name: 'Flower of Life Collective',
-		total_shares: 500000
+		admin_address,
 	})
 	return {
 		collective_address,
 		collective,
-	}
-}
-async function assert_get_collective_creator(player, t, { collective_address, collective_creator }) {
-	const get_collective_creator_result =
-		await player_call(player,
-			'get_collective_creator', {
-				collective_address,
-			}
-		)
-	const { Ok } = get_collective_creator_result
-	if (!Ok) {
-		t.fail(JSON.stringify(get_collective_creator_result))
-	}
-	t.deepEqual(Ok, {
-		collective_address,
-		collective_creator,
-	})
-	return {
-		collective_address,
-		collective_creator,
 	}
 }
 async function assert_get_collective_people(player, t, { collective_address, collective_people }) {
@@ -265,31 +238,6 @@ async function assert_set_collective_name(player, t, { collective_address, colle
 		collective: collective__result,
 	}
 }
-async function assert_set_collective_total_shares(player, t, { collective_address, collective, total_shares }) {
-	t.notEqual(collective.total_shares, total_shares)
-	const api_result = await player_call(player,
-		'set_collective_total_shares',
-		{
-			collective_address,
-			total_shares,
-		}
-	)
-	const { Ok } = api_result
-	if (!Ok) {
-		t.fail(JSON.stringify(api_result))
-	}
-	const {
-		collective_address: collective_address__result,
-		collective: collective__result,
-	} = Ok
-	t.deepEqual(
-		collective__result,
-		clone(collective, { total_shares }))
-	return {
-		collective_address,
-		collective: collective__result,
-	}
-}
 function _get_actions_result(player, t, collective_address) {
 	return player_call(player,
 		'get_actions',
@@ -304,7 +252,7 @@ function _create_collective_action(collective) {
 		status: 'Executed',
 		data: JSON.stringify(collective),
 		tag: 'create_collective',
-		action_intent: 'SystemAutomatic'
+		strategy: 'SystemAutomatic'
 	}
 }
 function _set_collective_name_action(name) {
@@ -313,16 +261,7 @@ function _set_collective_name_action(name) {
 		status: 'Executed',
 		data: JSON.stringify({ name }),
 		tag: 'set_collective_name',
-		action_intent: 'SystemAutomatic'
-	}
-}
-function _set_total_shares_action(total_shares) {
-	return {
-		op: 'SetCollectiveTotalShares',
-		status: 'Executed',
-		data: JSON.stringify({ total_shares }),
-		tag: 'set_collective_total_shares',
-		action_intent: 'SystemAutomatic'
+		strategy: 'SystemAutomatic'
 	}
 }
 function _add_collective_person_action(person_address) {
@@ -331,7 +270,7 @@ function _add_collective_person_action(person_address) {
 		status: 'Executed',
 		data: JSON.stringify({ person_address }),
 		tag: 'add_collective_person',
-		action_intent: 'SystemAutomatic'
+		strategy: 'SystemAutomatic'
 	}
 }
 async function wait_for(afn, timeout_ms = 5000, sleep_ms = 100) {
