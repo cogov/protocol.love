9962ccc74c2d7c77a3af91d0bdf3a4b3f663fdf8
Brian Takita <brian.takita@gmail.com>
2020-05-08 21:38:18 -0400

    protocol-love: utils.rs: rustdoc

--- zomes/protocol-love/code/src/utils.rs
+++ zomes/protocol-love/code/src/utils.rs
@@ -3,10 +3,12 @@ use holochain_wasm_utils::holochain_core_types::entry::AppEntryValue;
 use holochain_wasm_utils::holochain_persistence_api::cas::content::Address;
 use std::convert::TryFrom;
 
+/// Calls `hdk::utils::get_as_type` with a cloned `address`.
 pub fn get_as_type_ref<R: TryFrom<AppEntryValue>>(address: &Address) -> ZomeApiResult<R> {
 	hdk::utils::get_as_type(address.clone())
 }
 
+/// Prepends `tag` to a `ZomeApiError`.
 pub fn tag_error<T>(error: ZomeApiError, tag: &str) -> ZomeApiResult<T> {
 	let mut error_msg = "".to_owned();
 	error_msg.push_str(tag);
@@ -14,6 +16,7 @@ pub fn tag_error<T>(error: ZomeApiError, tag: &str) -> ZomeApiResult<T> {
 	return Err(error_msg.into());
 }
 
+/// Prepends `tag` to a `ZomeApiError` resulting from `ZomeApiResult`.
 pub fn t<T>(tag: &str, result: ZomeApiResult<T>) -> ZomeApiResult<T> {
 	match result {
 		Ok(val) => Ok(val),
